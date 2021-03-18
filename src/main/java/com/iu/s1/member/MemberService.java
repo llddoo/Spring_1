4181889519ed871ef9c8b3bd683c749d919e6152
이석준 <llddoo@naver.com>
2021-03-18 17:09:17 +0900

    03.18 param1

--- src/main/java/com/iu/s1/member/MemberService.java
+++ src/main/java/com/iu/s1/member/MemberService.java
@@ -9,6 +9,11 @@ public class MemberService {
 	@Autowired
 	private MemberDAO memberDAO;
 	
+	public int memberJoin(MemberDTO memberDTO)throws Exception{
+		return memberDAO.memberJoin(memberDTO);
+	}
+	
+	
 	public MemberDTO memberLogin(MemberDTO memberDTO) throws Exception {
 		
 		memberDTO = memberDAO.Login(memberDTO);
