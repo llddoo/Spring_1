b4052046d5565fa522a4414197850e22b9de3dab
이석준 <llddoo@naver.com>
2021-03-19 09:46:42 +0900

    03.19 Model 전 저장

--- src/main/java/com/iu/s1/member/MemberController.java
+++ src/main/java/com/iu/s1/member/MemberController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -23,10 +24,10 @@ public class MemberController {
 	
 	// memberJoin2 //	/member/memberJoin POST
 	@RequestMapping(value = "/member/memberJoin", method = RequestMethod.POST)
-	public void memberJoin2(MemberDTO memberDTO)throws Exception{
-
-		int result = memberService.memberJoin(memberDTO);
+	public void memberJoin(MemberDTO memberDTO)throws Exception{
 		
+		int result = memberService.memberJoin(memberDTO);
+		System.out.println("test");
 		System.out.println(result);
 	}
 	
@@ -47,7 +48,7 @@ public class MemberController {
 	
 	//memberLogin2 print
 	@RequestMapping(value = "/member/memberLogin", method = RequestMethod.POST)
-	public void memberLogin2(HttpServletRequest request) throws Exception {
+	public String memberLogin(HttpServletRequest request) throws Exception {
 		String id = request.getParameter("id");
 		String pw = request.getParameter("pw");
 		
@@ -57,6 +58,8 @@ public class MemberController {
 		memberDTO = memberService.memberLogin(memberDTO);
 		
 		System.out.println(memberDTO);
+		request.setAttribute("dto", memberDTO);
+		return "member/memberPage";
 		
 	}
 
