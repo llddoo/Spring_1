c728ed85d745cb9ec13daeb954c399f43397a252
llddoo <74532161+llddoo@users.noreply.github.com>
2021-03-22 11:03:17 +0900

    Update BankbookService.java

--- src/main/java/com/iu/s1/bankbook/BankbookService.java
+++ src/main/java/com/iu/s1/bankbook/BankbookService.java
@@ -11,6 +11,12 @@ public class BankbookService {
 	@Autowired
 	private BankBookDAO bankBookDAO;
 	
+	public BankbookService() {
+		//결합도가 높다, 강함
+		bankBookDAO = new BankBookDAO();
+		
+	}
+	
 	public BankBookDTO getSelect(BankBookDTO bankBookDTO) throws Exception{
 		return bankBookDAO.getSelect(bankBookDTO);
 	}
